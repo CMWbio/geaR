57feeeca0fc4a4d42009e53b2d5efd3548035ec7
CMWbio <christopher.ward@adelaide.edu.au>
2018-08-22 17:24:54 +0930

    started adding functions

---
+++ R/setup.R
@@ -0,0 +1,37 @@
+
+
+library(SeqArray)
+library(tibble)
+library(GenomicRanges)
+
+
+## files for analysis
+VCF <- "~/Desktop/Tree-TipR/PlutellaSNP.vcf.gz"
+GDS <- "~/Desktop/Tree-TipR/PlutellaSNP.GDS"
+
+#get header from VCF will be needed for windows
+contigMD <- seqVCF_Header("~/Desktop/Tree-TipR/PlutellaSNP.vcf.gz")$contig
+
+# converty VCF to GDS
+seqVCF2GDS(vcf.fn = "~/Desktop/Tree-TipR/PlutellaSNP.vcf.gz",
+           parallel = 6, out.fn = "~/Desktop/Tree-TipR/PlutellaSNP.GDS")
+
+## Parameters
+windowSize <- 100000
+step <- 100000
+minSites <- 1000
+
+
+
+
+
+
+
+#open GDS
+gds <- seqOpen(GDS)
+
+
+
+
+genotypes <-  seqGetData(gdsfile = gds, var.name = "genotype")
+
---
+++ R/windowMaker.R
@@ -0,0 +1,79 @@
+#' Sliding and tiled window ranges
+#'
+#' @description makes and tiled windows from data frame
+#'
+#' @details Authours: Chris Ward & Alastair Luddington
+#' generates tiled (no step size) and sliding (step size) ranges for analysis with other functions.
+#'
+#'
+#' @param contigMD data frame with contig metadata. Contains two columns: \code{ID} with scaffold names, \code{length} with scaffold length
+#' @param WindowSize \code{numeric} Window size in base pairs
+#' @param stepSize \code{numeric} Window step size in base pairs. Default is 0, specifies a tiled window.
+#'
+#'
+#' @return A \code{list} of \code{GRanges}
+#'
+#'
+#' @import SeqArray
+#' @importFrom GenomicRanges makeGRangesFromDataFrame
+#' @importFrom GenomicRanges slidingWindows
+#'
+#'
+#' @examples
+#'
+#' # Set up tiled windows
+#' windowSize <- 100000
+#' stepSize <- 0
+#'
+#' # make contig metadata across all contigs in VCF header
+#' contigMD <- seqVCF_Header(vcf_path)$contig
+#'
+#' # subset by desired contigs
+#' contigMD <- contigMD[contigMD$ID %in% c("chr1", "chr2"),]
+#'
+#'
+#' @export
+#' @rdname windowMaker
+
+windowMaker <- function(contigMD, windowSize, stepSize = 0){
+
+  # checks
+  if(!is.data.frame(contigMD)) stop("contigMD must be a data.frame or data.frame like object")
+  if(any(!colnames(contigMD) %in% c("ID", "length"))) stop("contigMD must be a data.frame or data.frame like object")
+  if(!windowSize >= stepSize) stop("windowSize must be greater than or equal to stepSize")
+  if(!is.numeric(windowSize) | !is.numeric(stepSize)) stop("windowSize and stepSize must be numerics")
+  stopifnot(windowSize < 0 | stepSize <= 0 )
+
+  # set step size for tiled
+  if(stepSize == 0) stepSize <- windowSize
+
+  # remove all contigs with length less than windowSize
+  contigMD <- contigMD[contigMD[["length"]] >= windowSize,]
+
+  # make contig range for scaffolds in contigMD and convert to GRanges object
+  contigRange <- makeGRangesFromDataFrame(data_frame(chr = contigMD[["ID"]], start = 1, strand = ".", end = contigMD[["length"]]))
+
+
+  ## List of windows
+  windowList <- lapply(seq(nrow(contigMD)), function(x){
+
+    ## Subsetting GRange object by chromosome
+    con <- contigRange[x]
+
+    ## Splitting into windows
+    G_range <- GenomicRanges::slidingWindows(x = con, width = windowSize, step = step)
+    G_range <- unlist(G_range) ## Need to unlist the slidingWindow object
+
+    ## Turning window-GRanges into GRangesList
+    G_range <- as(G_range,"GRangesList")
+    G_range
+
+  })
+
+  ## Returning list object
+  names(windowList) <- contigMD[["ID"]]
+  return(windowList)
+
+}
+
+t <- windowMaker(contigMD, windowSize)
